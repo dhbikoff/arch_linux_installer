0c1d205286797243be84b51501b50057a08e1829
Derek Bikoff <dhbikoff@gmail.com>
2017-06-11 07:40:16 -0400

    remove xorg-server-utils as it was removed from the main repo

--- arch_install.sh
+++ arch_install.sh
@@ -7,7 +7,7 @@ parted --script /dev/sda \
   mkpart ESP fat32 1MiB 513MiB \
   set 1 boot on \
   mkpart primary linux-swap 513MiB 2.5GiB \
-  mkpart primary ext4 2.5GiB 100% \
+  mkpart primary ext4 2.5GiB 100%
 
 mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
@@ -25,7 +25,7 @@ pacstrap /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt << EOF
-  ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+  ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
   hwclock --systohc --utc
   echo -e "en_US.UTF-8 UTF-8" >> /etc/locale.gen
   locale-gen
@@ -36,7 +36,7 @@ arch-chroot /mnt << EOF
   echo -e "default arch\ntimeout 4\n" > /boot/loader/loader.conf
   echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=/dev/sda3 rw" > /boot/loader/entries/arch.conf
   systemctl enable dhcpcd@enp0s3.service
-  pacman -S --noconfirm git tree xsel vim sudo bash-completion dkms xorg-server xorg-server-utils xorg-xinit linux-headers base-devel
+  pacman -S --noconfirm git tree xsel vim sudo bash-completion dkms xorg-server xorg-xinit linux-headers base-devel
   useradd -m -G wheel arch
   echo -e "arch\narch" | passwd arch
   echo -e "%wheel ALL=(ALL) ALL" >> /etc/sudoers
